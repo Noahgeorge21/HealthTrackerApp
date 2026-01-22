eab1fa1014a662db60159a75bb9c83fd8bb1a5d5
Noahgeorge21 <noahgeorge21@gmail.com>
2026-01-22 14:15:48 -0500

    Added "page"/"screen" logic

--- HealthTracker.py
+++ HealthTracker.py
@@ -1,6 +1,7 @@
 import sys 
 from datetime import datetime
 import time 
+import keyboard
 class Person:
     def __init__(self, name: str, age: int, sex: str, height: int):
         self.name = name
@@ -18,13 +19,15 @@ class Person:
         
     def view_all_records(self):
         if len(self.health_records) < 1:
-            print('No Health Records.')
+            print('No Health Records on File!')
         else:
+            print('WEIGHT--BMI--DATE--TIME')
             for i in self.health_records:
                 print(i)
                 
     def view_recent_record(self):
         if len(self.health_records) >= 1:
+            print('WEIGHT--BMI--DATE--TIME')
             print(self.health_records[-1])
         else:
             print('No Health Records on File!')
@@ -38,56 +41,88 @@ class HealthRecord:
     def __str__(self):
         return f'({self.weight}, {self.BMI}, {self.date})'
         
-user_db = {}
-current_person = None 
-
-while True:
-    print('MAIN MENU')
-    print()
-    print('Enter 1 to create a user')
-    print('Enter 2 to add a health record')
-    print('Enter 4 to view all health records')
-    print('Enter 5 to view most recent record')
-    print('Enter 3 to EXIT')
-    print()
-    selection = int(input('Enter your selection here: '))
-    if selection == 1:
-        name = input('enter name: ')
-        age = int(input('enter age: '))
-        sex = input('enter sex: ')
-        height = int(input('enter height (in): '))
-        current_person = Person(name, age, sex, height)
-        print('USER CREATED')
-        print('RETURNING TO MAIN MENU')
-        continue
-    elif selection == 2:
+current_person = None
+def view_recent_record():
+    while True:
         if current_person == None:
             print('ERROR -- USER NOT FOUND -- please create a user')
-            continue
+            break
+        else:
+            current_person.view_recent_record()
+            input('PRESS ANY KEY TO RETURN TO MAIN MENU')
+            break
+        
+def view_all_health_records():
+    global current_person
+    while True:
+        if current_person == None:
+            print('ERROR -- USER NOT FOUND -- please create a user')
+            break
+        else:
+            current_person.view_all_records()
+            input('PRESS ANY KEY TO RETURN TO MAIN MENU')
+            break
+        
+def add_health_record():
+    global current_person
+    while True:
+        if current_person == None:
+            print('ERROR -- USER NOT FOUND -- please create a user')
+            break
         else:
             weight = int(input('enter current weight: '))
             date = str(datetime.now())
             current_person.add_health_record(weight, date)
             print('HEALTH RECORD ADDED')
-            print('RETURNING TO MENU')
-            continue
-    elif selection == 4:
-        if current_person == None:
-            print('ERROR -- USER NOT FOUND -- please create a user')
-            continue
-        else:
-            print('weight, BMI, date')
-            current_person.view_all_records()
-    elif selection == 5:
-        if current_person == None:
-            print('ERROR -- USER NOT FOUND -- please create a user')
-            continue
-        else:
-            print('weight, BMI, date')
-            current_person.view_recent_record()
-        
-    elif selection == 3:
-        sys.exit('YOU HAVE NOW EXITED THE APP')
-    
-    
+            print('PRESS 1 TO RETURN TO MAIN MENU')
+            print('PRESS 2 TO ADD ANOTHER HEALTH RECORD')
+            choice = int(input('Enter: '))
+            if choice == 1:
+                break
+            elif choice == 2:
+                continue
+            else:
+                print('INVALID INPUT')
+                break
+            
+def create_user():
+    while True:
+        name = input('enter name: ')
+        age = int(input('enter age: '))
+        sex = input('enter sex: ')
+        height = int(input('enter height (in): '))
+        print('USER CREATED')
+        input('PRESS ANY KEY TO RETURN TO MAIN MENU')
+        break
+    return Person(name, age, sex, height)
 
+def main_menu():
+    global current_person
+    while True:
+        print('MAIN MENU')
+        print('Enter 1 to create a user')
+        print('Enter 2 to add a health record')
+        print('Enter 4 to view all health records')
+        print('Enter 5 to view most recent record')
+        print('Enter 3 to EXIT')
+        try:
+            selection = int(input('Enter your selection here: '))
+            
+            if selection == 1:
+                current_person = create_user()
+            elif selection == 2:
+                add_health_record()
+            elif selection == 3:
+                sys.exit('YOU HAVE NOW EXITED THE APP')
+            elif selection == 4:
+                view_all_health_records()
+            elif selection == 5:
+                view_recent_record()
+            else:
+                print('INVALID SELECTION')
+        except ValueError:
+            print('INVALID SELECTION -- PLEASE TRY AGAIN')
+
+            
+            
+main_menu()
